1f7eaf5fe25cbb51c44aa92d2060f7402a948b4f
Imran Hossen <freelancerimran81@gmail.com>
2022-04-01 10:45:36 +0600

    Grandpa updated

--- src/Components/Grandpa/Grandpa.js
+++ src/Components/Grandpa/Grandpa.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Grandpa.css'
 import Uncle from '../Uncle/Unlce';
 import Father from '../Father/Father';
 import Aunty from '../Aunty/Aunty'
 
 const Grandpa = () => {
-    const house = 7;
+    const [house, setHouse] = useState(1);
+    const handleBuyHouse = ()=>{
+        const newHouse = house + 1;
+        setHouse (newHouse)
+    }
+
     return (
-        <div className='grandpa' style={{ display: 'flex' }}>
-            <div>
+        <div className='grandpa'>
+            <h2>Grandpa</h2>
+            <p>House: {house}</p>
+            <button onClick={handleBuyHouse}>Buy A House</button>
+            <div style={{ display: 'flex' }}>
                 <Father house={house}></Father>
                 <Uncle house={house}></Uncle>
                 <Aunty house={house}></Aunty>
